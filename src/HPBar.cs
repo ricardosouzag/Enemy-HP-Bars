b225837d468260d5c7fedb966490d4ac5ac3438e
Ayugradow <32333973+Ayugradow@users.noreply.github.com>
2020-01-29 23:32:18 -0300

    Removed one canvas per enemy, that was dumb in retrospect

--- src/HPBar.cs
+++ src/HPBar.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
+using HutongGames.PlayMaker.Actions;
 using Modding;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,17 @@ namespace EnemyHPBar
     public class EnemyHPBar : Mod
     {
         private const string version = "1.2.1";
+
+        public static GameObject canvas;
+        
+        public static Sprite bg;
+        public static Sprite mg;
+        public static Sprite fg;
+        public static Sprite ol;
+        public static Sprite bossbg;
+        public static Sprite bossfg;
+        public static Sprite bossol;
+        
 
         public override string GetVersion()
         {
@@ -22,6 +34,23 @@ namespace EnemyHPBar
             ModHooks.Instance.OnEnableEnemyHook += Instance_OnEnableEnemyHook;
             ModHooks.Instance.OnRecieveDeathEventHook += Instance_OnRecieveDeathEventHook;
             UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            
+            
+
+            canvas = CanvasUtil.CreateCanvas(RenderMode.WorldSpace, new Vector2(16f, 9f));
+            canvas.GetComponent<Canvas>().sortingOrder = 1;
+            
+            
+
+            bg = CanvasUtil.CreateSprite(ResourceLoader.GetBackgroundImage(), 0, 0, 175, 19);
+            mg = CanvasUtil.CreateSprite(ResourceLoader.GetMiddlegroundImage(), 0, 0, 117, 10);
+            fg = CanvasUtil.CreateSprite(ResourceLoader.GetForegroundImage(), 0, 0, 117, 10);
+            ol = CanvasUtil.CreateSprite(ResourceLoader.GetOutlineImage(), 0, 0, 175, 19);
+            bossbg = CanvasUtil.CreateSprite(ResourceLoader.GetBossBackgroundImage(), 0, 0, 1, 1);
+            bossfg = CanvasUtil.CreateSprite(ResourceLoader.GetBossForegroundImage(), 0, 0, 960, 1);
+            bossol = CanvasUtil.CreateSprite(ResourceLoader.GetBossOutlineImage(), 0, 0, 966, 27);
+
+            Object.DontDestroyOnLoad(canvas);
 
             Log("Initialized EnemyHPBars");
         }
